fdd299d64155e6b8cd349332db9e83b000f9a3cc
bniscoise <benjamin.niscoise@consultants-solutec.fr>
2025-09-30 17:29:34 +0200

    exception handler

---
+++ letterboxdbackend/src/main/java/com/example/letterboxdbackend/controller/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.letterboxdbackend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+}
