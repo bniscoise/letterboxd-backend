6196d35216c68a073b39ac586845e617992d99ad
bniscoise <benjamin.niscoise@consultants-solutec.fr>
2025-10-07 11:28:39 +0200

    Dev (#4)
    * Restauration de mon travail local après corruption
    
    * git ...
    
    * reformatage + reglage pb maven
    
    * mon gitignore avait somehow disparu, c'est réparé

--- src/main/java/com/example/letterboxdbackend/dto/ImdbApiResponse.java
+++ src/main/java/com/example/letterboxdbackend/dto/ImdbApiResponse.java
@@ -4,110 +4,190 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class ImdbApiResponse {
-    
+
     @JsonProperty("titles")
     private List<ImdbTitle> titles;
-    
-    public ImdbApiResponse() {}
-    
+
+    public ImdbApiResponse() {
+    }
+
     public List<ImdbTitle> getTitles() {
         return titles;
     }
-    
+
     public void setTitles(List<ImdbTitle> titles) {
         this.titles = titles;
     }
-    
+
     // Classe interne pour représenter chaque titre
     public static class ImdbTitle {
         @JsonProperty("id")
         private String id;
-        
+
         @JsonProperty("type")
         private String type;
-        
+
         @JsonProperty("primaryTitle")
         private String primaryTitle;
-        
+
         @JsonProperty("originalTitle")
         private String originalTitle;
-        
+
         @JsonProperty("startYear")
         private Integer startYear;
-        
+
         @JsonProperty("endYear")
         private Integer endYear;
-        
+
         @JsonProperty("primaryImage")
         private PrimaryImage primaryImage;
-        
+
         @JsonProperty("rating")
         private Rating rating;
-        
+
+        @JsonProperty("plot")
+        private String plot;
+
         // Constructeurs
-        public ImdbTitle() {}
-        
+        public ImdbTitle() {
+        }
+
         // Getters et Setters
-        public String getId() { return id; }
-        public void setId(String id) { this.id = id; }
-        
-        public String getType() { return type; }
-        public void setType(String type) { this.type = type; }
-        
-        public String getPrimaryTitle() { return primaryTitle; }
-        public void setPrimaryTitle(String primaryTitle) { this.primaryTitle = primaryTitle; }
-        
-        public String getOriginalTitle() { return originalTitle; }
-        public void setOriginalTitle(String originalTitle) { this.originalTitle = originalTitle; }
-        
-        public Integer getStartYear() { return startYear; }
-        public void setStartYear(Integer startYear) { this.startYear = startYear; }
-        
-        public Integer getEndYear() { return endYear; }
-        public void setEndYear(Integer endYear) { this.endYear = endYear; }
-        
-        public PrimaryImage getPrimaryImage() { return primaryImage; }
-        public void setPrimaryImage(PrimaryImage primaryImage) { this.primaryImage = primaryImage; }
-        
-        public Rating getRating() { return rating; }
-        public void setRating(Rating rating) { this.rating = rating; }
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getPrimaryTitle() {
+            return primaryTitle;
+        }
+
+        public void setPrimaryTitle(String primaryTitle) {
+            this.primaryTitle = primaryTitle;
+        }
+
+        public String getOriginalTitle() {
+            return originalTitle;
+        }
+
+        public void setOriginalTitle(String originalTitle) {
+            this.originalTitle = originalTitle;
+        }
+
+        public Integer getStartYear() {
+            return startYear;
+        }
+
+        public void setStartYear(Integer startYear) {
+            this.startYear = startYear;
+        }
+
+        public Integer getEndYear() {
+            return endYear;
+        }
+
+        public void setEndYear(Integer endYear) {
+            this.endYear = endYear;
+        }
+
+        public PrimaryImage getPrimaryImage() {
+            return primaryImage;
+        }
+
+        public void setPrimaryImage(PrimaryImage primaryImage) {
+            this.primaryImage = primaryImage;
+        }
+
+        public Rating getRating() {
+            return rating;
+        }
+
+        public void setRating(Rating rating) {
+            this.rating = rating;
+        }
+
+        public String getPlot() {
+            return this.plot;
+        }
+
+        public void setPlot(String plot) {
+            this.plot = plot;
+        }
     }
-    
+
     public static class PrimaryImage {
         @JsonProperty("url")
         private String url;
-        
+
         @JsonProperty("width")
         private Integer width;
-        
+
         @JsonProperty("height")
         private Integer height;
-        
-        public PrimaryImage() {}
-        
-        public String getUrl() { return url; }
-        public void setUrl(String url) { this.url = url; }
-        
-        public Integer getWidth() { return width; }
-        public void setWidth(Integer width) { this.width = width; }
-        
-        public Integer getHeight() { return height; }
-        public void setHeight(Integer height) { this.height = height; }
+
+        public PrimaryImage() {
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public Integer getWidth() {
+            return width;
+        }
+
+        public void setWidth(Integer width) {
+            this.width = width;
+        }
+
+        public Integer getHeight() {
+            return height;
+        }
+
+        public void setHeight(Integer height) {
+            this.height = height;
+        }
     }
-    
+
     public static class Rating {
         @JsonProperty("aggregateRating")
         private Double aggregateRating;
-        
+
         @JsonProperty("voteCount")
         private Long voteCount;
-        
-        public Rating() {}
-        
-        public Double getAggregateRating() { return aggregateRating; }
-        public void setAggregateRating(Double aggregateRating) { this.aggregateRating = aggregateRating; }
-        
-        public Long getVoteCount() { return voteCount; }
-        public void setVoteCount(Long voteCount) { this.voteCount = voteCount; }
+
+        public Rating() {
+        }
+
+        public Double getAggregateRating() {
+            return aggregateRating;
+        }
+
+        public void setAggregateRating(Double aggregateRating) {
+            this.aggregateRating = aggregateRating;
+        }
+
+        public Long getVoteCount() {
+            return voteCount;
+        }
+
+        public void setVoteCount(Long voteCount) {
+            this.voteCount = voteCount;
+        }
     }
 }
--- src/main/java/com/example/letterboxdbackend/service/MovieService.java
+++ src/main/java/com/example/letterboxdbackend/service/MovieService.java
@@ -4,12 +4,12 @@ import com.example.letterboxdbackend.dto.ImdbApiResponse;
 import com.example.letterboxdbackend.dto.MovieDto;
 import com.example.letterboxdbackend.model.Movie;
 import com.example.letterboxdbackend.repository.MovieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.RestClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
